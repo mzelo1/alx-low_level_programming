9befd079c7fe2e53cf7be30c9b60dbbb084c2bf6
mzelo1 <oasismathare@gmail.com>
2023-08-12 00:56:45 -0700

    helo puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+
+/**
+ * testing usage of puts() function
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        /* return 0 for a successful program execution */
+        return (0);
+}
