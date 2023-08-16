3763aedb495de378ea9661405cc034bda5a4e1dd
mzelo1 <oasismathare@gmail.com>
2023-08-16 01:47:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *print_alphabet -Make the alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+chat ch;
+for (ch - 'a'; < -'z'; ch++)
+putchar(ch);
+putchar('\n');
+}
