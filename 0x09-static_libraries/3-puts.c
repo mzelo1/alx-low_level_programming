f023761cdd897d5464953c69be9f944dc9f5f198
mzelo1 <oasismathare@gmail.com>
2023-09-01 09:54:42 -0700

    0 static

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: the string to print
+ */
+void _puts(char *str)
+{
+    /* Function implementation here */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
